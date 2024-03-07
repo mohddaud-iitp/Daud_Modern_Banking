d6bd25b27d6fd00b9d38477bde12cab9b679138a
mohddaud-iitp <mdd397721@gmail.com>
2024-03-07 23:22:09 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer"; 
+import Stats from "./Stats";
+import GetStarted from "./GetStarted";
+import Testimonials from "./Testimonials";  
+
+
+export {
+    Navbar,
+    Billing,
+    CardDeal, 
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Button,
+    Hero,
+    FeedbackCard,
+    GetStarted
+}
